b6538e922f9768e743b8f250a0f32db3de637c4f
Juan <juan@gmail.com>
2025-11-27 17:32:35 -0300

    Clean project for Railway deployment

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Build script for Render
-./mvnw clean package -DskipTests
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Start script for Render
-java -Dspring.profiles.active=prod -jar target/*.jar
